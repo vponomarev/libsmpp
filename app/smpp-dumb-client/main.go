55d7986578e652840ea2b72676672cc6f3560e6f
VAPonomarev <vitaly.ponomarev@gmail.com>
2020-02-15 00:55:00 +0300

    + smpp-dumb-client: Added support of logLevel into config file, added support of -log LEVEL in command line

--- app/smpp-dumb-client/main.go
+++ app/smpp-dumb-client/main.go
@@ -11,11 +11,13 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type Config struct {
-	Remote string `yaml:"remote,omitempty"`
-	Bind   struct {
+	Remote   string `yaml:"remote,omitempty"`
+	LogLevel string `yaml:"logLevel,omitempty"`
+	Bind     struct {
 		SystemID   string `yaml:"systemID,omitempty"`
 		SystemType string `yaml:"systemType,omitempty"`
 		Password   string `yaml:"password,omitempty"`
@@ -41,7 +43,49 @@ type Config struct {
 	SendWindow int `yaml:"window"`
 }
 
+type Params struct {
+	LogLevel log.Level
+	Flags    struct {
+		LogLevel bool
+	}
+}
+
+func ProcessCMDLine() (p Params) {
+	// Set default
+	p.LogLevel = log.InfoLevel
+
+	var pv bool
+	var pvn string
+	for _, param := range os.Args[1:] {
+		if pv {
+			switch pvn {
+			// LogLevel
+			case "-log":
+				l, err := log.ParseLevel(param)
+				if err != nil {
+					l = log.InfoLevel
+					fmt.Print("Incorrect LogLevel [", param, "], set LogLevel to: ", l.String())
+				} else {
+					p.LogLevel = l
+					p.Flags.LogLevel = true
+				}
+			}
+		} else {
+			switch param {
+			case "-log":
+				pvn = param
+				pv = true
+			}
+		}
+	}
+	return p
+}
+
 func main() {
+	pParam := ProcessCMDLine()
+
+	fmt.Println("LogLevel:", pParam.LogLevel.String())
+
 	log.SetOutput(os.Stdout)
 	log.SetLevel(log.DebugLevel)
 
@@ -62,6 +106,18 @@ func main() {
 		} else {
 			fmt.Println("Error loading config file: ", err)
 			return
+		}
+	}
+
+	// Load LogLevel from config if present
+	if (len(config.LogLevel) > 0) && (!pParam.Flags.LogLevel) {
+		if l, err := log.ParseLevel(config.LogLevel); err == nil {
+			pParam.LogLevel = l
+
+			log.SetLevel(pParam.LogLevel)
+			log.WithFields(log.Fields{
+				"type": "smpp-client",
+			}).Warning("Override LogLevel to: ", pParam.LogLevel.String())
 		}
 	}
 
@@ -138,13 +194,13 @@ func main() {
 			log.WithFields(log.Fields{"type": "smpp-client", "SID": s.SessionID, "service": "outConnect", "action": "StatusUpdate"}).Warning(x.GetDirection().String(), ",", x.GetTCPState().String(), ",", x.GetSMPPState().String(), ",", x.GetSMPPMode().String(), ",", x.Error(), ",", x.NError())
 			if x.GetSMPPState() == libsmpp.CSMPPBound {
 
-				// Send packet
-				log.WithFields(log.Fields{"type": "smpp-client", "SID": s.SessionID, "service": "outConnect", "action": "SendPacket"}).Info("-")
-				s.Outbox <- rP
+				// Start packet submission
+				log.WithFields(log.Fields{"type": "smpp-client", "SID": s.SessionID, "service": "outConnect", "action": "SendPacket", "count": config.SendCount, "rate": config.SendRate}).Info("Start message bulk message submission")
+				go PacketSender(s, rP, uint(config.SendRate), uint(config.SendCount))
 			}
 
 		case x := <-s.Inbox:
-			log.WithFields(log.Fields{"type": "smpp-client", "SID": s.SessionID, "service": "outConnect", "action": "Inbox"}).Info(x)
+			log.WithFields(log.Fields{"type": "smpp-client", "SID": s.SessionID, "service": "outConnect", "action": "Inbox"}).Debug(x)
 
 			// Generate confirmation for DeliverSM
 			if x.Hdr.ID == libsmppConst.CMD_DELIVER_SM {
@@ -152,10 +208,69 @@ func main() {
 			}
 
 		case x := <-s.InboxR:
-			log.WithFields(log.Fields{"type": "smpp-client", "SID": s.SessionID, "service": "outConnect", "action": "InboxR"}).Info(x)
+			log.WithFields(log.Fields{"type": "smpp-client", "SID": s.SessionID, "service": "outConnect", "action": "InboxR"}).Debug(x)
 
 		case <-s.Closed:
 			log.WithFields(log.Fields{"type": "smpp-client", "SID": s.SessionID, "service": "outConnect", "action": "close"}).Warning("Connection is closed")
+			return
+		}
+	}
+}
+
+// Send messages
+func PacketSender(s *libsmpp.SMPPSession, p libsmpp.SMPPPacket, rate uint, cnt uint) {
+	var tick time.Duration
+	var tickCouter uint
+	var tickInfoModule uint
+	var blockSize uint
+	var msgLastSec uint
+	// For rate > 100 SMS/sec send send messages each 10 ms
+	if rate > 100 {
+		tick = 10 * time.Millisecond
+		blockSize = rate / 100
+		tickInfoModule = 100
+	} else if rate > 10 {
+		// For rate > 10 SMS/sec AND <= 100 SMS/sec send messages each 100 ms
+		tick = 100 * time.Millisecond
+		blockSize = rate / 10
+		tickInfoModule = 10
+	} else if rate > 1 {
+		// For rate > 1 SMS/sec AND <= 10 SMS/sec send message each 500 ms
+		tick = 500 * time.Millisecond
+		blockSize = rate / 2
+		tickInfoModule = 2
+	} else {
+		// Rate is 1 SMS/sec
+		tick = 1 * time.Second
+		blockSize = rate
+		tickInfoModule = 1
+	}
+
+	var done uint
+	c := time.Tick(tick)
+	for {
+		select {
+		case <-c:
+			tickCouter++
+			var i uint
+			for ; (i < blockSize) && (done < cnt); i++ {
+				s.Outbox <- p
+				msgLastSec++
+				done++
+			}
+			if done >= cnt {
+				fmt.Println("#Done!")
+				return
+			}
+
+			if tickCouter%tickInfoModule == 0 {
+				tx, rx := s.GetTrackQueueSize()
+				fmt.Println("[", s.SessionID, "] During last 1s: ", msgLastSec, " [MAX:", done, "][TX:", tx, "][RX:", rx, "]")
+				msgLastSec = 0
+			}
+
+		case <-s.Closed:
+			fmt.Println("#ClosedCH")
 			return
 		}
 	}
